f3dfaeb0e4d150a066bb83b8af6ad09656e4c395
Abdelaziz-bennacer <2abdelaziz.bennacer@gmail.com>
2024-05-06 13:24:19 +0200

    Initial Commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Abdelaziz Bennacer on 05/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Abdelaziz Bennacer on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
